1607541a6e79c3ffd0801329b2201e6c5c9de491
khf116 <khf116@khf.ru>
2022-11-28 10:18:15 +1000

    add 1st task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,11 @@
 function sum(a, b) {
-  /* ваш код */
+  if (isNaN(a) || typeof a !== 'number')
+    throw new TypeError('parameter "a" not a number');
+
+  if (isNaN(b) || typeof b !== 'number')
+    throw new TypeError('parameter "b" not a number');
+
+  return a + b;
 }
 
 module.exports = sum;
