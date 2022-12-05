6aed8933e28dfcbb3fe5092831e8118e98928cc5
khf116 <khf116@khf.ru>
2022-12-05 23:45:55 +1000

    исправил код для первого теста в задании 03-04

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -13,6 +13,11 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (req.method !== 'POST') {
+    res.statusCode = 501;
+    res.end('Not implemented');
+  }
+
   if (pathname.indexOf('/') > 0) {
     // console.log('directory is not supported');
     res.statusCode = 400;
@@ -21,48 +26,67 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
-  if (fs.existsSync(filepath)) {
-    // console.log('file exists');
-    res.statusCode = 409;
-    return res.end('file exists');
-  }
+  // if (fs.existsSync(filepath)) {
+  //   // console.log('file exists');
+  //   res.statusCode = 409;
+  //   return res.end('file exists');
+  // }
 
-  const limitSizeStream = new LimitSizeStream({
-    limit: MAX_LIMIT_SIZE,
-    isObjectMode: req.isObjectMode
+  fs.stat(filepath, (err, stats) => {
+    if (err && err.code !== 'ENOENT') {
+      // console.log(err);
+      res.statusCode = 500;
+      return res.end('some error');
+    }
+
+    if (stats) {
+      // console.log('file exists');
+      res.statusCode = 409;
+      return res.end('file exists');
+    }
+
+    setTimeout(() => {
+      writeFile(req, res, filepath);
+     }, 0);
+
   });
-  limitSizeStream.on('error', error => {
-    // console.log('limit stream error', error);
-    res.statusCode = 413;
-    res.write('limit error');
-  });
 
-  const fileStream = fs.createWriteStream(filepath);
+  // const limitSizeStream = new LimitSizeStream({
+  //   limit: MAX_LIMIT_SIZE,
+  //   isObjectMode: req.isObjectMode
+  // });
+  // limitSizeStream.on('error', error => {
+  //   // console.log('limit stream error', error);
+  //   res.statusCode = 413;
+  //   res.write('limit error');
+  // });
 
-  switch (req.method) {
-    case 'POST':
-      // console.log('begin', filepath);
+  // const fileStream = fs.createWriteStream(filepath);
 
-      pipeline(req, limitSizeStream, fileStream, (error) => {
-        if (error) {
-          // console.log(`pipeline error statusCode: ${res.statusCode}`, error);
-          fileStream.close(() => {
-            fs.rmSync(filepath);
-            // console.log(`${filepath} deleted`);
-          });
-        } else {
-          res.statusCode = 201;
-          res.write('done');
-        }
-        res.end();
-      });
+  // switch (req.method) {
+  //   case 'POST':
+  //     // console.log('begin', filepath);
 
-      break;
+  //     // pipeline(req, limitSizeStream, fileStream, (error) => {
+  //     //   if (error) {
+  //     //     // console.log(`pipeline error statusCode: ${res.statusCode}`, error);
+  //     //     fileStream.close(() => {
+  //     //       fs.rmSync(filepath);
+  //     //       // console.log(`${filepath} deleted`);
+  //     //     });
+  //     //   } else {
+  //     //     res.statusCode = 201;
+  //     //     res.write('done');
+  //     //   }
+  //     //   res.end();
+  //     // });
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+  //     break;
+
+  //   default:
+  //     res.statusCode = 501;
+  //     res.end('Not implemented');
+  // }
 });
 
 // server.on('clientError', (err, socket) => {
@@ -72,5 +96,42 @@ server.on('request', (req, res) => {
 // server.on('error', error => {
 //   console.log('server error', error);
 // });
+
+function writeFile(req, res, filepath) {
+  const limitSizeStream = new LimitSizeStream({
+    limit: MAX_LIMIT_SIZE,
+    isObjectMode: req.isObjectMode
+  });
+
+  const fileStream = fs.createWriteStream(filepath);
+
+  pipeline(req, limitSizeStream, fileStream, (error) => {
+    if (error) {
+      if (error.code === 'LIMIT_EXCEEDED')
+        res.statusCode = 413;
+      else
+        res.statusCode = 500;
+      res.end('error');
+
+      console.log(`pipeline error statusCode: ${res.statusCode}`, error);
+
+      setTimeout(() => {
+        fileStream.close(() => {
+          fs.unlink(filepath, (err) => {
+            if (err)
+              console.log(`delete error ${filepath}`);
+            else
+              console.log(`deleted ${filepath}`);
+
+            //res.end('error');
+          });
+        });
+      }, 0);
+    } else {
+      res.statusCode = 201;
+      res.end('done');
+    }
+  });
+}
 
 module.exports = server;
