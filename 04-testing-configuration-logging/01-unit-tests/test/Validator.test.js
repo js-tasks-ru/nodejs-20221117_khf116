cf8b5c619e0f82526bb31ef008dcd20cbfebb78c
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-12-06 16:04:46 +0300

    Added task Юнит тестирование

---
+++ 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
@@ -0,0 +1,22 @@
+const Validator = require('../Validator');
+const expect = require('chai').expect;
+
+describe('testing-configuration-logging/unit-tests', () => {
+  describe('Validator', () => {
+    it('валидатор проверяет строковые поля', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string',
+          min: 10,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({ name: 'Lalala' });
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('name');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('too short, expect 10, got 6');
+    });
+  });
+});
