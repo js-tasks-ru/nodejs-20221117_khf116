bc18191527cfdae38204c0e751e54ff61ccf41fa
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-12-11 21:06:52 +0300

    Added teacher solution for task: Файловый сервер - запись файла

---
+++ 03-streams/04-file-server-post/receiveFile.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
+
+module.exports = function receiveFile(filepath, req, res) {
+  if (req.headers['content-length'] > 1e6) {
+    res.statusCode = 413;
+    res.end('File is too big!');
+    return;
+  }
+
+  const writeStream = fs.createWriteStream(filepath, {flags: 'wx'});
+  const limitStream = new LimitSizeStream({limit: 1e6});
+
+  req
+      .pipe(limitStream)
+      .pipe(writeStream);
+
+  limitStream.on('error', (err) => {
+    if (err.code === 'LIMIT_EXCEEDED') {
+      res.statusCode = 413;
+      res.end('File is too big');
+
+      fs.unlink(filepath, (err) => {});
+      return;
+    }
+
+    console.error(err);
+
+    res.statusCode = 500;
+    res.end('Internal server error');
+
+    fs.unlink(filepath, (err) => {});
+  });
+
+  writeStream
+      .on('error', (err) => {
+        if (err.code === 'EEXIST') {
+          res.statusCode = 409;
+          res.end('File exists');
+          return;
+        }
+
+        console.error(err);
+
+        res.statusCode = 500;
+        res.end('Internal server error');
+
+        fs.unlink(filepath, (err) => {});
+      })
+      .on('finish', () => {
+        res.statusCode = 201;
+        res.end('File created');
+      });
+
+  req.on('aborted', () => {
+    fs.unlink(filepath, (err) => {});
+  });
+};
