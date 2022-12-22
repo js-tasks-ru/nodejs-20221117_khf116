d0360b11adc1a7652087c87537e83db0060bccc0
khf116 <khf116@khf.ru>
2022-12-22 14:39:44 +1000

    add task 06-02

--- 06-mongodb-mongoose/02-rest-api/app.js
+++ 06-mongodb-mongoose/02-rest-api/app.js
@@ -1,9 +1,48 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-const {productsBySubcategory, productList, productById} = require('./controllers/products');
-const {categoryList} = require('./controllers/categories');
+const {productsBySubcategory, productList, productById, productAdd} = require('./controllers/products');
+const {categoryList, categoryAdd} = require('./controllers/categories');
 
 const app = new Koa();
+
+async function mongoTest1() {
+  const mongoose = require('mongoose');
+  const schema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+  });
+
+  const Model = mongoose.model('Product', schema);
+
+  await mongoose.connect('mongodb://127.0.0.1:27017/mytest');
+
+  const product = await Model.find();
+  console.log(product);
+};
+// mongoTest1();
+
+
+async function mongoTest2() {
+  const mongoose = require('mongoose');
+  const schema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+  });
+
+  // const connection = mongoose.createConnection('mongodb://127.0.0.1:27017/mytest2');
+
+  const Model = connection.model('test1', schema);
+
+  const product = await Model.find();
+  console.log(product);
+};
+// mongoTest2();
+
+app.use(require('koa-bodyparser')());
 
 app.use(async (ctx, next) => {
   try {
@@ -25,6 +64,9 @@ const router = new Router({prefix: '/api'});
 router.get('/categories', categoryList);
 router.get('/products', productsBySubcategory, productList);
 router.get('/products/:id', productById);
+
+router.post('/categoryAdd', categoryAdd);
+router.post('/productAdd', productAdd);
 
 app.use(router.routes());
 
