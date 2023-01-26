f40f3158ea50da4258603c72abb1599f7298ab60
khf116 <khf116@khf.ru>
2023-01-26 16:45:07 +1000

    add task 08-02

--- 08-email/02-order/models/Order.js
+++ 08-email/02-order/models/Order.js
@@ -2,6 +2,36 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+
+  phone: {
+    type: String,
+    required: true,
+    validate: [
+      {
+        validator(value) {
+          return /\+?\d{6,14}/.test(value);
+        },
+        message: 'Неверный формат номера телефона.',
+      },
+    ],
+  },
+
+  address: {
+    type: String,
+    required: true,
+  },
+
 });
 
 module.exports = connection.model('Order', orderSchema);
